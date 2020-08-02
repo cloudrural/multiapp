5300221565cbe67427b97d595fbe79cddc279231
Bharath Pantala Subramanyam <bpantala@cisco.com>
2020-08-02 10:08:12 +0530

    Hello

--- build-images.sh
+++ build-images.sh
@@ -6,6 +6,26 @@ version=$2
 build_exec="/usr/bin/docker"
 set=$1
 
+# help session
+function _help_() {
+  echo "Usage:"
+  echo "Syntax:"
+  echo ""
+  echo "      ./build-images.sh <image-list> <version-app>"
+  echo ""
+  echo "eg:"
+  echo "      ./build-images.sh all 2020.1"
+  echo ""
+  echo "      1.First argument: images list (all, php, py, go, node)"
+  echo "       all = all images"
+  echo "       py = Python only"
+  echo "       go = Go App only"
+  echo "       node = Node App only"
+  echo "       php = PHP app only"
+  echo "      2. Second argument: Application version( 2020.1)"
+
+}
+
 # Building the images
 function _nodeapp_() {
 
@@ -59,5 +79,8 @@ if [ $set == all ]; then
    _pyapp_
    _nginx_
    echo "Successfully Built all images of Multiapp Deployment"
+
+if [ $set == "--help" ]; then
+   _help_
 
 fi;
